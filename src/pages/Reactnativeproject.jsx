f2686256637d1c39408cda2e7809098f1cb05a1c
victor oyiga <victoroyiga01@gmail.com>
2026-07-12 19:32:13 +0100

    starting with the react native to my student 4

--- src/pages/Reactnativeproject.jsx
+++ src/pages/Reactnativeproject.jsx
@@ -1,3 +1,4 @@
+import "./styles/styles.css";
 import { Link } from "react-router-dom";
 import projects from "./data/reactnativeproject";
 
@@ -6,6 +7,17 @@ const ReactNativeProject = () => {
         <div>
             <h1 className="page-title" style={{ display: "flex", textAlign: "center", flexDirection: "column" }}>React Native Project</h1>
 
+            <div className="courses-container">
+                 {projects.map((project) => (
+                    /* Wrap the card in a Link component */
+                    <Link to={`/project/${project.id}`} key={project.id} style={{ textDecoration: 'none', color: 'inherit' }}>
+                        <div className="course-card">
+                            <h2 className="title">{project.title}</h2>
+                            <h3 className="subtitle">{project.subtitle}</h3>
+                        </div>
+                    </Link>
+                ))}
+            </div>
             <div style={{ marginTop: "30px" }}>
                 <Link to="/" style={{ fontWeight: "bold", textTransform: "uppercase", textDecoration: "none", color: "black", textAlign: "end" }}>
                     ← Back to Home

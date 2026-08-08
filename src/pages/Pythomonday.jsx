b3f2c4e93cfd104038d04db982c1395e7d8c41fb
victor oyiga <victoroyiga01@gmail.com>
2026-08-08 21:07:10 +0100

    working on monday friday:version 2

--- src/pages/Pythomonday.jsx
+++ src/pages/Pythomonday.jsx
@@ -5,10 +5,19 @@ const Pythomonday = () => {
     return (
         <div className="container">
             <h1 className="page-title" style={{ display: "flex", textAlign: "center", flexDirection: "column" }}>Python Monday, Wednesday and Friday Class Projects and Group Members</h1>
-       
-            
-       
-       
+
+            <div className="courses-container">
+
+
+            </div>
+
+            <div style={{ marginTop: "30px" }}>
+                <Link to="/" style={{ fontWeight: "bold", textTransform: "uppercase", textDecoration: "none", color: "black", textAlign: "end" }}>
+                    ← Back to Home
+                </Link>
+            </div>
+
+
         </div>
     )
 }

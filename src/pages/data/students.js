b4a8500643d010a54ce252856b24a1301d8535a1
victor oyiga <victoroyiga01@gmail.com>
2026-09-23 14:54:00 +0100

    link the project

--- src/pages/data/students.js
+++ src/pages/data/students.js
@@ -1,257 +1,123 @@
-
-
-
 const students = [
   {
     projectId: 1,
     group: "Group 1",
-    members: [
-       "Franklin Chukuma",
-       "Folu"
-
-    ],
+    members: ["Franklin Chukuma", "Folu"],
   },
 
   {
     projectId: 2,
     group: "Group 2",
-    members: [
-      
-      "Omokehinde Joseph ",
-      "peters Tobi",
-      "Adsina Ayolamola"
-    ],
+    members: ["Omokehinde Joseph ", "peters Tobi", "Adsina Ayolamola"],
   },
 
   {
     projectId: 3,
     group: "Group 3",
-    members: [
-      
-      "",
-      "",
-      ""
-    ],
-    
+    members: ["", "", ""],
   },
   {
     projectId: 4,
     group: "Group 4",
-    members: [
-      
-      "Abioye Mathew",
-    ],
-    
+    members: ["Abioye Mathew"],
   },
   {
     projectId: 5,
     group: "Group 5",
-    members: [
-      
-      "Bakara Samayyah",
-      "Erhurhe Harrietta"
-     
-    ],
-    
+    members: ["Bakara Samayyah", "Erhurhe Harrietta"],
   },
   {
     projectId: 6,
     group: "Group 6",
-    members: [
-      
-     "Bakara Abdusalay",
-    ],
-    
+    members: ["Bakara Abdusalay"],
   },
   {
     projectId: 7,
     group: "Group 7",
-    members: [
-      "Akinbody emmanuel",
-      "Adaramobu Renhard",
-    ],
-    
+    members: ["Akinbody emmanuel", "Adaramobu Renhard"],
   },
   {
     projectId: 8,
     group: "Group 9",
-    members: [
-      
-      "",
-      "",
-      ""
-    ],
-    
+    members: ["", "", ""],
   },
   {
     projectId: 10,
     group: "Group 10",
-    members: [
-      
-      "",
-      "",
-      ""
-    ],
-    
+    members: ["", "", ""],
   },
   {
     projectId: 11,
     group: "Group 11",
-    members: [
-      
-      "",
-      "",
-      ""
-    ],
-    
+    members: ["", "", ""],
   },
   {
     projectId: 12,
     group: "Group 12",
-    members: [
-      
-      "",
-      "",
-      ""
-    ],
-    
+    members: ["", "", ""],
   },
   {
     projectId: 13,
     group: "Group 13",
-    members: [
-      
-      "",
-      "",
-      ""
-    ],
-    
+    members: ["", "", ""],
   },
   {
     projectId: 14,
     group: "Group 14",
-    members: [
-      
-      "",
-      "",
-      ""
-    ],
-    
+    members: ["", "", ""],
   },
   {
     projectId: 15,
     group: "Group 15",
-    members: [
-      
-      "",
-      "",
-      ""
-    ],
-    
+    members: ["", "", ""],
   },
   {
     projectId: 16,
     group: "Group 16",
-    members: [
-      
-      "",
-      "",
-      ""
-    ],
-    
+    members: ["", "", ""],
   },
   {
     projectId: 17,
     group: "Group 17",
-    members: [
-      
-      "",
-      "",
-      ""
-    ],
-    
+    members: ["", "", ""],
   },
   {
     projectId: 18,
     group: "Group 18",
-    members: [
-      
-      "",
-      "",
-      ""
-    ],
-    
+    members: ["", "", ""],
   },
   {
     projectId: 19,
     group: "Group 19",
-    members: [
-      
-     "modelbelu Emmanuella",
-      "Ajani Baliqees",
-      "Abdlsalam Adam"
-    ],
-    
+    members: ["modelbelu Emmanuella", "Ajani Baliqees", "Abdlsalam Adam"],
   },
   {
     projectId: 20,
     group: "Group 20",
-    members: [
-      
-      "",
-      "",
-      ""
-    ],
-    
+    members: ["", "", ""],
   },
   {
     projectId: 21,
     group: "Group 21",
-    members: [
-      
-      "",
-      "",
-      ""
-    ],
-    
+    members: ["", "", ""],
   },
   {
     projectId: 22,
     group: "Group 22",
-    members: [
-      
-      "",
-      "",
-      ""
-    ],
-    
+    members: ["", "", ""],
   },
   {
     projectId: 23,
     group: "Group 23",
-    members: [
-      
-      "",
-      "",
-      ""
-    ],
-    
+    members: ["", "", ""],
   },
   {
     projectId: 24,
     group: "Group 24",
-    members: [
-      
-      "",
-      "",
-      ""
-    ],
-    
+    members: ["osa uadia", "onjeabor vanessa ", ""],
   },
+  
+ 
 ];
 
 export default students;
-
-
-
-

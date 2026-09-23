c85865829f24e9daa2ea3d6197d97920c2c4b733
victor oyiga <victoroyiga01@gmail.com>
2026-09-23 13:44:23 +0100

    link the project

--- src/pages/data/students.js
+++ src/pages/data/students.js
@@ -23,7 +23,61 @@ const students = [
     ],
   },
 
- 
+  {
+    projectId: 3,
+    group: "Group 3",
+    members: [
+      
+      "",
+      "",
+      ""
+    ],
+    
+  },
+  {
+    projectId: 4,
+    group: "Group 4",
+    members: [
+      
+      "modelbelu Emmanuella",
+      "Ajani Baliqees",
+      "Abdlsalam Adam"
+    ],
+    
+  },
+  {
+    projectId: 5,
+    group: "Group 5",
+    members: [
+      
+      "",
+      "",
+      ""
+    ],
+    
+  },
+  {
+    projectId: 6,
+    group: "Group 6",
+    members: [
+      
+      "",
+      "",
+      ""
+    ],
+    
+  },
+  {
+    projectId: 7,
+    group: "Group 7",
+    members: [
+      
+      "",
+      "",
+      ""
+    ],
+    
+  },
 ];
 
 export default students;
